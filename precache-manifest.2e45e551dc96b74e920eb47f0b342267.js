e992029a50eff490370dbbea68b9134bf6e82909
Tom Casper <tmcasper@gmail.com>
2020-10-06 19:41:28 -0400

    Updates

---
+++ precache-manifest.2e45e551dc96b74e920eb47f0b342267.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f2fdaf3525d53771efd872c13a49a9",
+    "url": "/gh-pages-react/index.html"
+  },
+  {
+    "revision": "af8fa0b341abf18d3c76",
+    "url": "/gh-pages-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac0f0bcf17447404accc",
+    "url": "/gh-pages-react/static/js/2.471f3d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gh-pages-react/static/js/2.471f3d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8fa0b341abf18d3c76",
+    "url": "/gh-pages-react/static/js/main.e0349740.chunk.js"
+  },
+  {
+    "revision": "9cf7c9cd95c86e4913b2",
+    "url": "/gh-pages-react/static/js/runtime-main.60b3b0ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
